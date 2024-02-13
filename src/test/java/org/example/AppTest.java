b63392f236dfe0498e0aca0b77a52621a08e62d9
Marija <gestuverteja.marija@gmail.com>
2024-02-13 19:38:51 +0200

    added dummy test to illustrate how branch works

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,6 +1,15 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AppTest {
+
+    @Test
+    public void dummyTest(){
+
+        Assertions.assertTrue(true);
+    }
 
 }
 
